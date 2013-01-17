0107a7f23092ea835de66f675e81b5b82ffe2f5e
Flavio Costa <flavio.cdc@gmail.com>
2013-01-17 00:09:27 -0200

    Refactoring
    * moving the main program to function 'main()'
    * extracting the GSAT login to a self contained object

--- gsat.js
+++ gsat.js
@@ -1,17 +1,4 @@
 (function() {
-  print("Enter the number of restarts");
-  retries = parseInt(readline());
-
-  print("Enter the number of flips");
-  flips = parseInt(readline());
-
-  print("Enter the number of literals");
-  num_literals = parseInt(readline());
-
-  print("Now enter the clauses.");
-  print("(3 numbers, separated by spaces, indicating the index of the literal)");
-  print("Type 'stop', to stop adding clauses");
-
   function Literal(clause, clause_index) {
     var index = Math.abs(clause[clause_index]);
     var not = clause[clause_index] < 0;
@@ -25,7 +12,7 @@
   function Formula(num_literals) {
     this.clauses = new Array();
 
-    this.ls = new Array();
+    this.ls = [];
     this.ls.length = num_literals;
 
     this.evalClause = function(clause, env) {
@@ -56,19 +43,116 @@
     }
   };
 
-  formula = new Formula(num_literals);
+  function main() {
+    print("Enter the number of restarts");
+    var retries = parseInt(readline());
 
-  var line = readline();
-  do {
-    var numbers = line.split(" ").map(function(it) { return parseInt(it) });
-    print(numbers[0], numbers[1], numbers[2]);
-    formula.clauses.push(numbers);
+    print("Enter the number of flips");
+    var flips = parseInt(readline());
 
-    line = readline();
-  } while (line != "stop");
+    print("Enter the number of literals");
+    var num_literals = parseInt(readline());
 
-  //print_2d_array(formula.clauses);
+    print("Now enter the clauses.");
+    print("(3 numbers, separated by spaces, indicating the index of the literal)");
+    print("Type 'stop', to stop adding clauses");
 
+    var formula = new Formula(num_literals);
+
+    var line = readline();
+    do {
+      var numbers = line.split(" ").map(function(it) { return parseInt(it) });
+      print(numbers[0], numbers[1], numbers[2]);
+      formula.clauses.push(numbers);
+
+      line = readline();
+    } while (line != "stop");
+
+    var gsat = new GSAT(formula, retries, flips);
+    var truth = gsat.run();
+
+    print("Verdade: ", truth);
+  }
+
+  function GSAT(formula, retries, flips) {
+    function generate_random_attribution() {
+      var literals = [];
+
+      for (var i = 0; i < formula.ls.length; i++) {
+        literals[i] = random_bool();
+      }
+
+      formula.ls = literals;
+    }
+
+    function hillclimb() {
+      var currently_sat = formula.satisfied(formula.ls);
+
+      //print("currently_sat = ", currently_sat, " env = ", formula.ls);
+
+      var more_sat = [];
+      var max_sat = currently_sat;
+      
+      for (var i = 0; i < formula.ls.length; i++) {
+        var new_ls = formula.ls.slice(0); // cloning the array
+        new_ls[i] = !formula.ls[i]; 
+
+        var sat = formula.satisfied(new_ls);
+        if (sat >= currently_sat) {
+          //print("Encontrei uma atribuicao que atende melhor");
+          more_sat.push({ satisfied: sat, env : new_ls });
+          max_set = sat > max_sat ? sat : max_sat;
+        }
+      }
+
+      //print("Total de atribs que atendem melhor: ", more_sat.length);
+
+      more_sat.filter(function(tuple) {
+        return tuple.satisfied >= max_sat;
+      });
+
+      //print("Total de atribs que atendem melhor depois de filtrada: ", more_sat.length);
+      
+      return more_sat;
+    }
+
+    this.run = function() {
+      for (var i = 0; i < retries; i++) {
+        generate_random_attribution(formula);
+        print("Generating random attr: ", formula.ls);
+
+        for (var j = 0; j < flips; j++) {
+          //print("Tentando verificar verdade");
+          if (formula.apply()) {
+            //print("Encontrei!");
+            return formula.ls;
+          } else {
+            more_sat = hillclimb(formula);
+            var rand_int = random_int(more_sat.length - 1); // generate random num between 0 and len(more_sat) - 1
+            
+            //print("more_sat.length = ", more_sat.length, " / rand_int = ", rand_int);
+            var tuple = more_sat[rand_int];
+            formula.ls = tuple.env;
+          }
+        }
+
+        print("Com a atribuicao inicial e o numero flips nao foi possivel encontrar uma solucao, tentar novamente");
+      }
+
+      return forumla.ls;
+    }
+  }
+
+  // utilities
+  function print_2d_array(array) {
+    for (var i = 0; i < array.length; i++) {
+      for (var j = 0; j < array[i].length; j++) {
+        write(array[i][j], " ");
+      }
+      print();
+    }
+  }
+  
   function random_bool() {
     var f = Math.random();
     return f >= 0.5;
@@ -78,83 +162,7 @@
     return Math.floor(Math.random()*(max+1));
   }
 
-  function generate_random_attribution(formula) {
-    var literals = new Array();
-    literals.length = formula.ls.length;
-
-    for (var i = 0; i < literals.length; i++) {
-      literals[i] = random_bool();
-    }
-
-    formula.ls = literals;
-  }
-
-  function hillclimb(formula) {
-    var currently_sat = formula.satisfied(formula.ls);
-
-    //print("currently_sat = ", currently_sat, " env = ", formula.ls);
-
-    var more_sat = [];
-    var max_sat = currently_sat;
-    
-    for (var i = 0; i < formula.ls.length; i++) {
-      var new_ls = formula.ls.slice(0); // cloning the array
-      new_ls[i] = !formula.ls[i]; 
-
-      var sat = formula.satisfied(new_ls);
-      if (sat >= currently_sat) {
-        //print("Encontrei uma atribuicao que atende melhor");
-        more_sat.push({ satisfied: sat, env : new_ls });
-        max_set = sat > max_sat ? sat : max_sat;
-      }
-    }
-
-    //print("Total de atribs que atendem melhor: ", more_sat.length);
-
-    more_sat.filter(function(tuple) {
-      return tuple.satisfied >= max_sat;
-    });
-
-    //print("Total de atribs que atendem melhor depois de filtrada: ", more_sat.length);
-    
-    return more_sat;
-  }
-
-  function gsat(formula) {
-    for (var i = 0; i < retries; i++) {
-      generate_random_attribution(formula);
-      print("Generating random attr: ", formula.ls);
-
-      for (var j = 0; j < flips; j++) {
-        //print("Tentando verificar verdade");
-        if (formula.apply()) {
-          //print("Encontrei!");
-          return formula.ls;
-        } else {
-          more_sat = hillclimb(formula);
-          var rand_int = random_int(more_sat.length - 1); // generate random num between 0 and len(more_sat) - 1
-          
-          //print("more_sat.length = ", more_sat.length, " / rand_int = ", rand_int);
-          var tuple = more_sat[rand_int];
-          formula.ls = tuple.env;
-        }
-      }
-
-      print("Com a atribuicao inicial e o numero flips nao foi possivel encontrar uma solucao, tentar novamente");
-    }
-  }
-
-  function print_2d_array(array) {
-    for (var i = 0; i < array.length; i++) {
-      for (var j = 0; j < array[i].length; j++) {
-        write(array[i][j], " ");
-      }
-      print();
-    }
-  }
-
-  gsat(formula);
-
-  print("Verdade: ", formula.ls);
+  // main
+  main();
 
 })();
