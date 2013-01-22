de6a4830275552440a43a4cc4bf4ce200a6bb464
Flavio Costa <flavio.cdc@gmail.com>
2013-01-22 10:26:38 -0200

    Can't generate clauses with repeated literals

--- input-gen.js
+++ input-gen.js
@@ -1,10 +1,10 @@
 load("utils.js");
 
 function main() {
-  var retries = 999;
+  var retries = 100;
   var flips = 5;
   var num_literals = 5;
-  var clauses = 20;
+  var clauses = 22;
 
   print(retries);
   print(flips);
@@ -12,8 +12,16 @@ function main() {
 
   for (var i = 0; i < clauses; i++) {
     var rand1 = random_int(num_literals - 1) * (random_bool() ? 1 : -1);
-    var rand2 = random_int(num_literals - 1) * (random_bool() ? 1 : -1);
-    var rand3 = random_int(num_literals - 1) * (random_bool() ? 1 : -1);
+
+    var rand2 = 0;
+    do {
+      rand2 = random_int(num_literals - 1) * (random_bool() ? 1 : -1);
+    } while (Math.abs(rand2) == Math.abs(rand1));
+
+    var rand3 = 0;
+    do {
+      rand3 = random_int(num_literals - 1) * (random_bool() ? 1 : -1);
+    } while (Math.abs(rand1) == Math.abs(rand3) || Math.abs(rand2) == Math.abs(rand3));
 
     print(rand1, rand2, rand3);
   }
